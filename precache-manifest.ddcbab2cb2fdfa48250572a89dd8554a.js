5b58cda7b1189dfff664d09c036ba5465466d6b7
paulangcrackers <bmariepaula121@gmail.com>
2019-11-15 15:09:12 +0800

    Updates

---
+++ precache-manifest.ddcbab2cb2fdfa48250572a89dd8554a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "664833c64d6ead17b4b6ed03a976d3dc",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "8c4050abd7d9a01d50db",
+    "url": "/calculator/static/css/main.a2cbb84f.chunk.css"
+  },
+  {
+    "revision": "f02bd9cd0c7f51b42ac6",
+    "url": "/calculator/static/js/2.bd6a4d13.chunk.js"
+  },
+  {
+    "revision": "8c4050abd7d9a01d50db",
+    "url": "/calculator/static/js/main.c13d38fb.chunk.js"
+  },
+  {
+    "revision": "71b0ecf05784cf43a89f",
+    "url": "/calculator/static/js/runtime-main.6d31f3e4.js"
+  }
+]);
